94e14608efa9b12fc5b64afdd576231f5333a767
Sizwe MAMBA <smamba@e5r3p1.jnb.42.fr>
2016-10-24 12:21:00 +0200

    adding libft

--- push.sh
+++ push.sh
@@ -3,6 +3,8 @@ git rm -f vector/*.o vector/.*
 git rm -f imod/*.o vector/.*
 git rm -f matrix/*.o matrix/.*
 git rm -f manager/*.o manager/.*
+rm -f libft/.*
+rm -f libft/*.o
 
 git add .
 git status 
